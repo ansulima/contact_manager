a22e94721eaef1dabc25ac16887977ec9666fbaf
Anderson Lima <freitaslimaanderson@gmail.com>
2023-04-26 23:31:10 -0300

    feat: Adicionado rotas no arquivo routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :contacts
+ 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -10,4 +10,7 @@ Rails.application.routes.draw do
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
 
+
+  resources :contacts
+  resources :users , only: [:new, :create]
 end
